f8cefbef0103bc23c1082a1c3e1ebb70e773c943
Sidharth461 <sidharthkamble911@gmail.com>
2026-08-19 10:39:53 +0530

    simulator: gracefully shutdown on ctrl-c instead of running forever

--- simulator/main.go
+++ simulator/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strconv"
 	"time"
 
@@ -38,7 +39,11 @@ func main() {
 		go v.Run(client, interval)
 	}
 
-	select {}
+	// select {}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	log.Println("shutting down...")
 }
 
 func getenv(key, def string) string {
